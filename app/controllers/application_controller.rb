a40f55bde7642dd516d532efd68abf6c31d54032
Ola Bergefall <ola.bergefall@gmail.com>
2018-05-15 12:22:59 +1000

    CP-152: Fixed logout. Properly logout via keycloak.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   # This is done by Devise. Cf.
   # https://github.com/plataformatec/devise/wiki/How-To:-Change-the-redirect-path-after-destroying-a-session-i.e.-signing-out
   def after_sign_out_path_for(resource_or_scope)
-    if not request[:redirect_uri].nil? then
-      request[:redirect_uri]
-    else
-      ''
-    end
+       "#{CfiOauthProvider::Application.config.keycloak_logout_uri}?redirect_uri=#{CfiOauthProvider::Application.config.keycloak_arvados_url}"
   end
 
 end
