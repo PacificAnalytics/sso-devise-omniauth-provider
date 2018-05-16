cda9549c2a2d2b5f854d1623aa755cf7838b3610
Ola Bergefall <ola.bergefall@gmail.com>
2018-05-16 10:28:20 +1000

    CP-152: Cleanup config. No duplicate information.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # This is done by Devise. Cf.
   # https://github.com/plataformatec/devise/wiki/How-To:-Change-the-redirect-path-after-destroying-a-session-i.e.-signing-out
   def after_sign_out_path_for(resource_or_scope)
-       "#{CfiOauthProvider::Application.config.keycloak_logout_uri}?redirect_uri=#{CfiOauthProvider::Application.config.keycloak_arvados_url}"
+       "#{CfiOauthProvider::Application.config.keycloak_logout_uri}?redirect_uri=#{CfiOauthProvider::Application.config.default_link_url}"
   end
 
 end
