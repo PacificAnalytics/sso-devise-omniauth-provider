52683dbe3ab15e870ef9de750eee1f8caca69eb6
Ola Bergefall <ola.bergefall@gmail.com>
2018-05-16 16:55:24 +1000

    CP-152: revert to old behavior if not using keycloak.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
   # This is done by Devise. Cf.
   # https://github.com/plataformatec/devise/wiki/How-To:-Change-the-redirect-path-after-destroying-a-session-i.e.-signing-out
   def after_sign_out_path_for(resource_or_scope)
-       "#{CfiOauthProvider::Application.config.keycloak_logout_uri}?redirect_uri=#{CfiOauthProvider::Application.config.default_link_url}"
+    if CfiOauthProvider::Application.config.use_keycloak
+      return "#{CfiOauthProvider::Application.config.keycloak_logout_uri}?redirect_uri=#{CfiOauthProvider::Application.config.default_link_url}"
+    elsif not request[:redirect_uri].nil? then
+      request[:redirect_uri]
+    else
+      ''
+    end
   end
 
 end
